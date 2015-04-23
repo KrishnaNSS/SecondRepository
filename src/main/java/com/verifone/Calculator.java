9f649268653d8764d3b2e0b9d46fd754a39f51e6
Krishna NSS <krishna.nss@verifone.com>
2015-04-23 12:59:24 +0530

    Added comments.

--- src/main/java/com/verifone/Calculator.java
+++ src/main/java/com/verifone/Calculator.java
@@ -1,21 +1,28 @@
 package com.verifone;
 
+/**This class is to demonstrate a simple calculator*/
+
 public class Calculator {
-	public int add(int a, int b)
+	
+	/**Adds two integers*/
+	public int add(/**First value */int a, /**Second value */int b)
 	{
 		return a+b;
 	}
 	
+	/**Subtracts two integers*/
 	public int subtract(int a, int b)
 	{
 		return a-b;
 	}
 	
+	/**Multiplies two integers*/
 	public int multiply(int a, int b)
 	{
 		return a*b;
 	}
 	
+	/**Devides two integers*/
 	public int devide(int a, int b)
 	{
 		return a/b;
